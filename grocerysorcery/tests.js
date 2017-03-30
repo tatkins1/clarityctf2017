4746ab64fd11a1736e3043be450ad800b2380623
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-03-30 04:14:33 -0500

    Implement all error tests

--- grocerysorcery/tests.js
+++ grocerysorcery/tests.js
@@ -50,6 +50,7 @@
 		}
 
 		var IS_TESTING = true;
+		var allErrors = [];
 		var caughtErrors = [];
 
 		window.onerror = err => {
@@ -70,31 +71,90 @@
 				var msg = caughtErrors.shift() || false;
 				var err = msg ? ERROR_REVERSE[msg] : 'NONE';
 				errList.push(err);
+				allErrors.push(err);
 			}
+			caughtErrors = [];
 			return errList.toString();
 		}
 
-		communityField.value = 'as';
+		communityField.value = 'BRONZEVILLE';
 		communitySubmit.click();
+		zipcodeField.value = 60602;
+		zipcodeSubmit.click();
 		$.post(CTF_URL + '15a', {
 			silent: SECRETS['IS_SILENT'],
 			team: SECRETS['TEAM_API_KEY'],
-			answer: getCaughtError()
+			answer: getCaughtError(2)
 		}).then((res) => {
 			displayResult(res);
 		});
 
-		communityField.value = 'aasdasds';
+		communityField.value = '';
 		communitySubmit.click();
-		//console.log(getCaughtError());
+		zipcodeField.value = '';
+		zipcodeSubmit.click();
+		$.post(CTF_URL + '15b', {
+			silent: SECRETS['IS_SILENT'],
+			team: SECRETS['TEAM_API_KEY'],
+			answer: getCaughtError(2)
+		}).then((res) => {
+			displayResult(res);
+		});
 
-		communityField.value = 'AS';
+		communityField.value = 'LOGAN.SQUARE.NORTH';
 		communitySubmit.click();
-		//console.log(getCaughtError());
+		communityField.value = 'EAST.GARFIELD,PARK.';
+		communitySubmit.click();
+		communityField.value = 'DUNNING/';
+		communitySubmit.click();
+		communityField.value = '/DU/NNING///';
+		communitySubmit.click();
+		$.post(CTF_URL + '15c', {
+			silent: SECRETS['IS_SILENT'],
+			team: SECRETS['TEAM_API_KEY'],
+			answer: getCaughtError(4)
+		}).then((res) => {
+			displayResult(res);
+		});
 
-		communityField.value = 'LOGAN SQUARE';
+		communityField.value = 'Auburn Gresham';
 		communitySubmit.click();
-		console.log(getCaughtError(3));
+		var g = getCaughtError(1)
+		console.log(g)
+		$.post(CTF_URL + '15d', {
+			silent: SECRETS['IS_SILENT'],
+			team: SECRETS['TEAM_API_KEY'],
+			answer: g
+		}).then((res) => {
+			displayResult(res);
+		});
+
+		zipcodeField.value = 'Chicago';
+		zipcodeSubmit.click();
+		zipcodeField.value = '100 Michigan Street';
+		zipcodeSubmit.click();
+		zipcodeField.value = '60600';
+		zipcodeSubmit.click();
+		zipcodeField.value = '606000';
+		zipcodeField.value = '6060';
+		zipcodeSubmit.click();
+		zipcodeField.value = '-60607';
+		zipcodeSubmit.click();
+		$.post(CTF_URL + '15e', {
+			silent: SECRETS['IS_SILENT'],
+			team: SECRETS['TEAM_API_KEY'],
+			answer: getCaughtError(6)
+		}).then((res) => {
+			displayResult(res);
+		});
+
+		$.post(CTF_URL + '15f', {
+			silent: SECRETS['IS_SILENT'],
+			team: SECRETS['TEAM_API_KEY'],
+			answer: allErrors.toString()
+		}).then((res) => {
+			displayResult(res);
+		});
 
 		IS_TESTING = false;
 
